75cf8314440c32cb84bb4fedc121513466d8274b
gillchristian <gillchristiang@gmail.com>
2017-12-31 03:07:43 +0100

    Add postinstall

--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 # d-scripts
+
+> handle node project tasks with no configuration
+
+## Install
+
+```bash
+$ yarn add --dev d-scripts
+```
---
+++ scripts/postinstall.js
@@ -0,0 +1,31 @@
+// TODO: add opt out of this feature
+
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
+const promisify = require('util').promisify
+const writeFile = promisify(fs.writeFile)
+
+const pkgPath = path.join(process.cwd(), 'package.json')
+
+const pkg = require(pkgPath)
+
+Object.assign(pkg.scripts, {
+  format: 'd-scripts format',
+  lint: 'd-scripts lint',
+  test: 'd-scripts test',
+})
+
+writeFile(pkgPath, JSON.stringify(pkg, null, 2), 'utf8')
+  .then(() => {
+    console.log()
+    console.log(`  ${chalk.bold('package.json')} scripts updated!`)
+    console.log()
+  })
+  .catch(err => {
+    console.log(
+      `Error while trying to add scripts to ${chalk.bold('package.json')}.`
+    )
+    console.log()
+    console.error(err)
+  })
