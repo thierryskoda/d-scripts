39502d17400d92c2e94c2dbbeff6e1617a14a18f
gillchristian <gillchristiang@gmail.com>
2017-12-31 12:41:18 +0100

    Add prompt to confirm init

--- scripts/init.js
+++ scripts/init.js
@@ -1,30 +1,63 @@
 const fs = require('fs')
 const path = require('path')
+const { promisify } = require('util')
 const chalk = require('chalk')
-const promisify = require('util').promisify
+const inquirer = require('inquirer')
+
 const writeFile = promisify(fs.writeFile)
 
-const pkgPath = path.join(process.cwd(), 'package.json')
+function writePkg() {
+  const pkgPath = path.join(process.cwd(), 'package.json')
 
-const pkg = require(pkgPath)
+  // eslint-disable-next-line import/no-dynamic-require,global-require
+  const pkg = require(pkgPath)
 
-Object.assign(pkg.scripts, {
-  format: 'd-scripts format',
-  lint: 'd-scripts lint',
-  test: 'd-scripts test',
-})
+  Object.assign(pkg.scripts, {
+    format: 'd-scripts format',
+    lint: 'd-scripts lint',
+    test: 'd-scripts test',
+    'test:cover': 'd-scripts test --coverage',
+  })
 
-writeFile(pkgPath, JSON.stringify(pkg, null, 2), 'utf8')
-  .then(() => {
+  return writeFile(pkgPath, JSON.stringify(pkg, null, 2), 'utf8').then(() => {
     console.log()
     console.log(
-      `  ${chalk.bold('d-scripts')} available scripts to ${chalk.bold(
+      `  Added ${chalk.bold('d-scripts')} available scripts to ${chalk.bold(
         'package.json'
       )}!`
     )
     console.log()
   })
+}
+
+const proceedMessage = `The following scripts will be added to your package.json:
+
+    ${chalk.cyan('lint')}         ${chalk.reset('- d-scripts lint')}
+    ${chalk.cyan('format')}       ${chalk.reset('- d-scripts format')}
+    ${chalk.cyan('test')}         ${chalk.reset('- d-scripts test')}
+    ${chalk.cyan('test:cover')}   ${chalk.reset('- d-scripts test --coverage')}
+
+  If you already have some of these scripts defined they will be overriden.
+  Are you sure you want to continue?`
+
+const questions = [
+  {
+    type: 'confirm',
+    name: 'proceed',
+    default: false,
+    message: proceedMessage,
+  },
+]
+
+inquirer
+  .prompt(questions)
+  .then(({ proceed }) => {
+    if (proceed) {
+      writePkg()
+    }
+  })
   .catch(err => {
+    console.log()
     console.log(
       `Error while trying to add scripts to ${chalk.bold('package.json')}.`
     )
